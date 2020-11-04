177f6a105520fa5d7a4bc9310dfd9d698958645b
pahalsonu <pahalsonu@gmail.com>
2020-11-04 18:52:33 +0530

    user as customer registeration completed and email token is being sent in template

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const port = process.env.port || 5000;
 
 app.use(express.json());
 
-// app.use('/users', userRoutes);
+// importing routes
+const customerRoutes = require('./routes/customer/index')
+app.use('/api/customer', customerRoutes);
+
 
 app.listen(port, ()=>{
 console.log(`Server is running at ${port}`)
--- routes/customer/index.js
+++ routes/customer/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const pug = require("pug");
 
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
+const randomstring = require("randomstring");
+const { body, validationResult } = require('express-validator');
 const { route } = require("../admin");
-
+const { AES } = require("crypto-js");
+const Mailer = require("../../controllers/mailController");
+const config = require('../../config/index.json')
+//Importing DB Models
+const Customer = require("../../models/Customer");
+const Admin = require("../../models/Admin");
 
 /*
 route : /api/customer/register
@@ -18,16 +26,55 @@ router.post('/register', [
     body("email", "Please Enter Valid EMail").isEmail(),
     body("role", "Role is neccessary").notEmpty(),
     body("password", "Password is neccessary").isLength({ min: 6 })
-    
-], 
-async(req, res) =>{
-    try{
 
-    }catch(err){
-        console.error(err);
-        res.status(500).json({'Error' : 'Servor Error'})
-    }
-})
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            let { name, email, role} = req.body;
+            let customer = await Customer.findOne({ email });
+            let admin = await Admin.findOne({ email });
+            if (customer) {
+                return res.status(500).json({ Error: `${email} is already registered as a Customer` });
+            }
+            if (admin) {
+                return res.status(500).json({ Error: `${email} is already registered as an Admin` });
+            }
+            //hashing pass
+            const saltRounds = 10;
+            const salt = await bcrypt.genSalt(saltRounds);
+            let password = await bcrypt.hash(req.body.password, salt);
+            const emailtoken = randomstring.generate();
+
+            customer = new Customer({ name, email, role, emailtoken, password })
+            await customer.save();
+            const verifyURL = `localhost:5000/api/customer/verify/${emailtoken}`;
+            const subject = 'New Product Email Verification';
+            const html = pug.renderFile(__dirname + '/email.pug');
+            Mailer(email, subject, html);
+            //Prepare the Payload for access token 
+            const payload = {
+                customer: customer._id,
+                role: customer.role
+            };//Create jwt access token
+            const token = await jwt.sign(payload, config.SECRET_KEY, { expiresIn: 50000 });
+            const cypherToken = AES.encrypt(token, config.CRYPTO_KEY).toString();
+            res.status(200).json({ token: cypherToken });
+
+
+
+
+
+
+        } catch (err) {
+
+            console.error(err);
+            res.status(500).json({ 'Error': 'Servor Error' })
+        }
+    })
 
 
 module.exports = router;
